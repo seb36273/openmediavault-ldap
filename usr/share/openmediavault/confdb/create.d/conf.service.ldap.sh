216da0e10ddfa832001ca5db99313bd17878ee64
root <root@localhost.localdomain>
2023-03-31 15:39:59 +0200

    First adapt to Openmediavault 6 with backward compatibility to 5

--- usr/share/openmediavault/confdb/create.d/conf.service.ldap.sh
+++ usr/share/openmediavault/confdb/create.d/conf.service.ldap.sh
@@ -28,7 +28,7 @@ set -e
 # <config>
 #   <services>
 #     <ldap>
-#       <enable>0</enable>
+#       <enable>1</enable>
 #       <host></host>
 #       <port>389</port>
 #       <enablessl>0</enablessl>
@@ -37,6 +37,8 @@ set -e
 #       <rootbindpw></rootbindpw>
 #       <usersuffix></usersuffix>
 #       <groupsuffix></groupsuffix>
+#       <machinesuffix></machinesuffix>
+#       <idmapsuffix></idmapsuffix>
 #       <enablepam>0|1</enablepam>
 #       <extraoptions></extraoptions>
 #       <extraclientoptions></extraclientoptions>
@@ -46,7 +48,7 @@ set -e
 ########################################################################
 if ! omv_config_exists "/config/services/ldap"; then
 	omv_config_add_node "/config/services" "ldap"
-	omv_config_add_key "/config/services/ldap" "enable" "0"
+	omv_config_add_key "/config/services/ldap" "enable" "1"
 	omv_config_add_key "/config/services/ldap" "host" ""
 	omv_config_add_key "/config/services/ldap" "port" "389"
 	omv_config_add_key "/config/services/ldap" "enablessl" "0"
@@ -55,6 +57,8 @@ if ! omv_config_exists "/config/services/ldap"; then
 	omv_config_add_key "/config/services/ldap" "rootbindpw" ""
 	omv_config_add_key "/config/services/ldap" "usersuffix" "ou=Users"
 	omv_config_add_key "/config/services/ldap" "groupsuffix" "ou=Groups"
+	omv_config_add_key "/config/services/ldap" "machinesuffix" "ou=Computers"
+	omv_config_add_key "/config/services/ldap" "idmapuffix" "ou=idmap"
 	omv_config_add_key "/config/services/ldap" "enablepam" "1"
 	omv_config_add_key "/config/services/ldap" "extraoptions" ""
 	omv_config_add_key "/config/services/ldap" "extraclientoptions" ""
